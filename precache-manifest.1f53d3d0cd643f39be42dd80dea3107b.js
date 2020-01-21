87dbca3f98194cd805e7fdfa558b1806a723f8aa
Bpedley <v3rtex12@gmail.com>
2020-01-22 00:35:53 +0300

    Updates

---
+++ precache-manifest.1f53d3d0cd643f39be42dd80dea3107b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5faaab7b5a1d1e9cf9f0b3c4012df58e",
+    "url": "/frontend-javascript-test/index.html"
+  },
+  {
+    "revision": "686a783bcda20092a2cf",
+    "url": "/frontend-javascript-test/static/css/2.4058c193.chunk.css"
+  },
+  {
+    "revision": "7bf7021668e3dd596400",
+    "url": "/frontend-javascript-test/static/css/main.b9759114.chunk.css"
+  },
+  {
+    "revision": "686a783bcda20092a2cf",
+    "url": "/frontend-javascript-test/static/js/2.9a2093ef.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/frontend-javascript-test/static/js/2.9a2093ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7bf7021668e3dd596400",
+    "url": "/frontend-javascript-test/static/js/main.78025c95.chunk.js"
+  },
+  {
+    "revision": "a42b9031d340fe3cdb21",
+    "url": "/frontend-javascript-test/static/js/runtime-main.55eda523.js"
+  }
+]);
